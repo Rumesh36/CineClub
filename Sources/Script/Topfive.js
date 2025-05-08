9a6b3e3920d1c684e83fc15e3ad3744b42a83c6f
Rumesh36 <rrumesh54@email.com>
2025-05-09 00:10:53 +0530

    added to 5 movies

---
+++ Sources/Script/Topfive.js
@@ -0,0 +1,17 @@
+const indiaBtn = document.getElementById("ind")
+const WorldBtn = document.getElementById("world")
+
+const indiaimg = document.getElementsByClassName("india")[0]
+const worldimg = document.getElementsByClassName("world")[0]
+
+indiaBtn.addEventListener("click",()=>{
+   
+    indiaimg.style.display="block"
+    worldimg.style.display="none"
+})
+
+WorldBtn.addEventListener("click",()=>{
+     indiaimg.style.display="none"
+    worldimg.style.display="block"
+})
+
