8869252e8a725a97038805ca9cbfb8c030c7d93a
varan027 <varanlucky02@gmail.com>
2025-05-09 00:23:32 +0530

    added navbar.js

---
+++ Sources/Script/navbar.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu-bar");
+const sidebar = document.getElementById("side-bar");
+const xmark = document.getElementById("x-mark");
+const lists = document.getElementsByTagName("ul")[0];
+
+menu.addEventListener("click", ()=>{
+  sidebar.style.display = "block";
+})
+
+xmark.addEventListener("click", ()=>{
+  sidebar.style.display = "none";
+})
