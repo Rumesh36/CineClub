6afd006549a16b409d1db360e848b7e1a7424d40
Rumesh36 <rrumesh54@email.com>
2025-05-11 03:19:25 +0530

    created category page

---
+++ Sources/Script/category.js
@@ -0,0 +1,17 @@
+let category={ 
+    horror :document.getElementsByClassName("horror"),
+    action :document.getElementsByClassName("action"),
+    scifi : document.getElementsByClassName("sci-fi"),
+    romance : document.getElementsByClassName("romance"),
+
+}
+
+const select =document.getElementById("category-select")
+
+select.addEventListener("change",()=>{
+
+    const selected = select.value
+    const elementch
+
+
+})
