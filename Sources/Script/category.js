f18bd3b9699e8676094eed459e3946fab21e9111
Rumesh36 <rrumesh54@email.com>
2025-05-11 20:20:57 +0530

    category page completed

--- Sources/Script/category.js
+++ Sources/Script/category.js
@@ -1,7 +1,7 @@
 let category={ 
     horror :document.getElementsByClassName("horror"),
     action :document.getElementsByClassName("action"),
-    scifi : document.getElementsByClassName("sci-fi"),
+    scifi : document.getElementsByClassName("scifi"),
     romance : document.getElementsByClassName("romance"),
 
 }
@@ -11,7 +11,23 @@ const select =document.getElementById("category-select")
 select.addEventListener("change",()=>{
 
     const selected = select.value
-    const elementch
+    console.log(selected);
+    
+    for(let key in category) //here key returns the property like horror ,action
+        {
+        for(let item of category[key])// and cat[key] return value like document.getemlebyid
+         {  
+            console.log(item);
+            item.style.display="none"
+                
+         }
+        }
+
+      for(let item of category[selected]){
+        console.log(item);
+        
+        item.style.display="flex"
+      }  
 
 
 })
